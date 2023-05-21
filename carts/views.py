5e40aba6daddd199bd184be62361b51c98c2e75b
Shayan <expertshayan@gmail.com>
2023-05-21 23:08:52 +0500

    Add Product Variations & Variation Manager

--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from store.models import Product
+from store.models import Product, Variation
 from .models import Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
@@ -11,12 +11,20 @@ def _cart_id(request):
     return cart
 
 def add_cart(request, product_id):
-    color = request.GET.get('color')
-    size = request.GET.get('size')
-    
-    return HttpResponse(color + ' ' + size)
-    
     product = Product.objects.get(id=product_id)
+    product_variation = []
+    
+    if request.method == 'POST':
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+            
+            try:
+                variation = Variation.objects.get(product=product, variation_category__iexact=key, variation_value__iexact=value)
+                product_variation.append(variation)
+            except:
+                pass
+            
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
     except Cart.DoesNotExist:
